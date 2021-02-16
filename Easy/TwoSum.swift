50c8faf6ca0710749b51ab7124955fe88fa3a0e2
irnafitriani <irna.fitriani@gmail.com>
2021-02-17 01:20:51 +0700

    Create TwoSum.swift

---
+++ Easy/TwoSum.swift
@@ -0,0 +1,12 @@
+class Solution {
+    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+        var dict = [Int: Int]()
+        for (i,num) in nums.enumerated() {
+            if let lastIndex = dict[target - num] {
+                return [lastIndex,i]
+            }
+            dict[num] = i
+        }
+        return [0,0]
+    }
+}
