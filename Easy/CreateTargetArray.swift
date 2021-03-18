5e4174d35051fa5239fd0f8109d6b872d8dd3370
irnafitriani <irna.fitriani@gmail.com>
2021-03-19 00:10:07 +0700

    Create CreateTargetArray.swift

---
+++ Easy/CreateTargetArray.swift
@@ -0,0 +1,13 @@
+//Create Target Array in the Given Order
+
+class Solution {
+    func createTargetArray(_ nums: [Int], _ index: [Int]) -> [Int] {
+        var target = [Int]()
+        
+        for i in 0..<nums.count {
+            target.insert(nums[i], at: index[i])
+        }
+        
+        return target
+    }
+}
